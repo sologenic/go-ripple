a9f24fb4c699a1d73e363a3ba9f89856cab6e69f
Norbert van Nobelen <norbert.van.nobelen2@gmail.com>
2024-04-24 07:58:31 -0700

    WIP: Analysis of rippled transactions

--- data/trade.go
+++ data/trade.go
@@ -20,147 +20,143 @@ type Trade struct {
 }
 
 /*
-	           {
+Keep these notes for future reference:
+The rippled trades should use the ripple_state nodes to show the full trade path.
+function newRippleState is a first attempt to structure this data, however for now a different solution is used in the code to collapse the offers in certain scenarios into a single line.
+This single line, if it works well (more records need to be analyzed to see if it's correct), will be ported into the newTrade function once verified, however it verification fails,
+the newRippleState function work will be usable.
+
+		           {
+		                "ModifiedNode": {
+		                    "FinalFields": {
+		                        "Balance": {
+		                            "currency": "434F524500000000000000000000000000000000",
+		                            "issuer": "rrrrrrrrrrrrrrrrrrrrBZbvji",
+		                            "value": "-5818.434290071079"
+		                        },
+		                        "Flags": 2228224,
+		                        "HighLimit": {
+		                            "currency": "434F524500000000000000000000000000000000",
+		                            "issuer": "rQ3VRvcp4taJrkp6b2WJW3TPP5zxsFp5Ld",
+		                            "value": "500000000"
+		                        },
+		                        "HighNode": "8",
+		                        "LowLimit": {
+		                            "currency": "434F524500000000000000000000000000000000",
+		                            "issuer": "rcoreNywaoz2ZCQ8Lg2EbSLnGuRBmun6D",
+		                            "value": "0"
+		                        },
+		                        "LowNode": "154"
+		                    },
+		                    "LedgerEntryType": "RippleState",
+		                    "LedgerIndex": "5E7AB54AE88612123DD989C3AEF14B008BD5C38E4066E053C24DB6A87986486B",
+		                    "PreviousFields": {
+		                        "Balance": {
+		                            "currency": "434F524500000000000000000000000000000000",
+		                            "issuer": "rrrrrrrrrrrrrrrrrrrrBZbvji",
+		                            "value": "-5817.434290071079"
+		                        }
+		                    },
+		                    "PreviousTxnID": "5172D1444AA33ADA8DACBE994008E5777F53DCA043F936F26EBAB4F49F9EFD4D",
+		                    "PreviousTxnLgrSeq": 87420168
+		                }
+		            },
+
+					e.g.:
+					abs(modifiednode.finalfields.balance.value) - abs(previousfields.balance.value) = paid amount to user in finalfields.highlimit.issuer
+
+					Does that also work for offer? If so we can get rid of bookdirectory function and solve this a lot easier
+
+					So the offer part looks like:
+
+					{
 	                "ModifiedNode": {
 	                    "FinalFields": {
-	                        "Balance": {
-	                            "currency": "434F524500000000000000000000000000000000",
-	                            "issuer": "rrrrrrrrrrrrrrrrrrrrBZbvji",
-	                            "value": "-5818.434290071079"
+	                        "Account": "rhqTdSsJAaEReRsR27YzddqyGoWTNMhEvC",
+	                        "BookDirectory": "5C8970D155D65DB8FF49B291D7EFFA4A09F9E8A68D9974B25A08B7604E87EA3A",
+	                        "BookNode": "0",
+	                        "Flags": 0,
+	                        "OwnerNode": "2",
+	                        "Sequence": 71723515,
+	                        "TakerGets": {
+	                            "currency": "534F4C4F00000000000000000000000000000000",
+	                            "issuer": "rsoLo2S1kiGeCcn6hCUXVrCpGMWLrRrLZz",
+	                            "value": "39.81129921936846"
 	                        },
-	                        "Flags": 2228224,
-	                        "HighLimit": {
-	                            "currency": "434F524500000000000000000000000000000000",
-	                            "issuer": "rQ3VRvcp4taJrkp6b2WJW3TPP5zxsFp5Ld",
-	                            "value": "500000000"
-	                        },
-	                        "HighNode": "8",
-	                        "LowLimit": {
-	                            "currency": "434F524500000000000000000000000000000000",
-	                            "issuer": "rcoreNywaoz2ZCQ8Lg2EbSLnGuRBmun6D",
-	                            "value": "0"
-	                        },
-	                        "LowNode": "154"
+	                        "TakerPays": "9767400"
 	                    },
-	                    "LedgerEntryType": "RippleState",
-	                    "LedgerIndex": "5E7AB54AE88612123DD989C3AEF14B008BD5C38E4066E053C24DB6A87986486B",
+	                    "LedgerEntryType": "Offer",
+	                    "LedgerIndex": "032ABC1F85D88CC888D4872DD7D0AE45CFF79FEDC4EF4C0C6C76800FE00AF56C",
 	                    "PreviousFields": {
-	                        "Balance": {
-	                            "currency": "434F524500000000000000000000000000000000",
-	                            "issuer": "rrrrrrrrrrrrrrrrrrrrBZbvji",
-	                            "value": "-5817.434290071079"
-	                        }
+	                        "TakerGets": {
+	                            "currency": "534F4C4F00000000000000000000000000000000",
+	                            "issuer": "rsoLo2S1kiGeCcn6hCUXVrCpGMWLrRrLZz",
+	                            "value": "40.759361979"
+	                        },
+	                        "TakerPays": "10000000"
 	                    },
-	                    "PreviousTxnID": "5172D1444AA33ADA8DACBE994008E5777F53DCA043F936F26EBAB4F49F9EFD4D",
-	                    "PreviousTxnLgrSeq": 87420168
+	                    "PreviousTxnID": "FD076550A85CC2FBEF394752C45FA128435A059EA768678412255825FAD155F1",
+	                    "PreviousTxnLgrSeq": 87420135
 	                }
 	            },
 
-				e.g.:
-				abs(modifiednode.finalfields.balance.value) - abs(previousfields.balance.value) = paid amount to user in finalfields.highlimit.issuer
-
-				Does that also work for offer? If so we can get rid of bookdirectory function and solve this a lot easier
-
-				So the offer part looks like:
-
-				{
-                "ModifiedNode": {
-                    "FinalFields": {
-                        "Account": "rhqTdSsJAaEReRsR27YzddqyGoWTNMhEvC",
-                        "BookDirectory": "5C8970D155D65DB8FF49B291D7EFFA4A09F9E8A68D9974B25A08B7604E87EA3A",
-                        "BookNode": "0",
-                        "Flags": 0,
-                        "OwnerNode": "2",
-                        "Sequence": 71723515,
-                        "TakerGets": {
-                            "currency": "534F4C4F00000000000000000000000000000000",
-                            "issuer": "rsoLo2S1kiGeCcn6hCUXVrCpGMWLrRrLZz",
-                            "value": "39.81129921936846"
-                        },
-                        "TakerPays": "9767400"
-                    },
-                    "LedgerEntryType": "Offer",
-                    "LedgerIndex": "032ABC1F85D88CC888D4872DD7D0AE45CFF79FEDC4EF4C0C6C76800FE00AF56C",
-                    "PreviousFields": {
-                        "TakerGets": {
-                            "currency": "534F4C4F00000000000000000000000000000000",
-                            "issuer": "rsoLo2S1kiGeCcn6hCUXVrCpGMWLrRrLZz",
-                            "value": "40.759361979"
-                        },
-                        "TakerPays": "10000000"
-                    },
-                    "PreviousTxnID": "FD076550A85CC2FBEF394752C45FA128435A059EA768678412255825FAD155F1",
-                    "PreviousTxnLgrSeq": 87420135
-                }
-            },
-
-			And substraction FInalFields - PreviousFields seems to give us the received amount using takerGets
-
+				And substraction FInalFields - PreviousFields seems to give us the received amount using takerGets
 */
 func newRippleState(txm *TransactionWithMetaData, i int) (*Trade, error) {
-	// 	_, final, previous, action := txm.MetaData.AffectedNodes[i].AffectedNode()
-	// 	v, ok := final.(*RippleState)
-	// 	if !ok || action == Created {
-	// 		return nil, nil
-	// 	}
-	// 	p := previous.(*RippleState)
-	// 	// if p != nil && p.TakerGets == nil || p.TakerPays == nil {
-	// 	// 	// Some "micro" offer consumptions don't change both balances!
-	// 	// 	return nil, nil
-	// 	// }
-	// 	paid := v.HighLimit
-	// 	if p.HighLimit != nil {
-	// 		var err error
-	// 		paid, err = p.HighLimit.Subtract(v.HighLimit)
-	// 		if err != nil {
-	// 			return nil, err
-	// 		}
-	// 	}
-	// 	got := v.LowLimit
-	// 	if p.HighLimit != nil {
-	// 		var err error
-	// 		got, err = p.HighLimit.Subtract(v.HighLimit)
-	// 		if err != nil {
-	// 			return nil, err
-	// 		}
-	// 	}
+	_, final, previous, action := txm.MetaData.AffectedNodes[i].AffectedNode()
+	v, ok := final.(*RippleState)
+	if !ok || action == Created {
+		return nil, nil
+	}
+	p := previous.(*RippleState)
+	// if p != nil && p.TakerGets == nil || p.TakerPays == nil {
+	// 	// Some "micro" offer consumptions don't change both balances!
+	// 	return nil, nil
+	// }
+	paid := v.HighLimit
+	if p.HighLimit != nil {
+		var err error
+		paid, err = p.HighLimit.Subtract(v.HighLimit)
+		if err != nil {
+			return nil, err
+		}
+	}
+	paid.Issuer = v.LowLimit.Issuer
+	got := v.Balance
+	got.Issuer = v.LowLimit.Issuer
+	if p.Balance != nil {
+		var err error
+		got, err = p.Balance.Subtract(v.Balance)
+		if err != nil {
+			return nil, err
+		}
+		got = got.Abs()
+	}
 
-	// 	/*
-	// Offer: FinalFields.BookDirectory.String()
-	// 	if price, err := priceFromBookDirectory(rplTrade.FinalFields.BookDirectory.String(), rplTrade.Paid.Currency.String(), rplTrade.Got.Currency.String()); err == nil {
-
-	// 	*/
-	// 	o:=&Offer{
-	// 		FinalFields
-	// 	}
-	// 	// got, err := p.TakerGets.Subtract(v.TakerGets)
-	// 	// if err != nil {
-	// 	// 	return nil, err
-	// 	// }
-	// 	trade := &Trade{
-	// 		LedgerSequence:   txm.LedgerSequence,
-	// 		TransactionIndex: txm.MetaData.TransactionIndex,
-	// 		TransactionType:  txm.GetTransactionType().String(),
-	// 		Op:               "Modify",
-	// 		Paid:             paid,
-	// 		Got:              got,
-	// 		Giver:            v.HighLimit.Issuer,
-	// 		Taker:            txm.Transaction.GetBase().Account,
-	// 		// FinalFields:      v, => We Required this data as *Offer to calculate the price
-	// 		// PreviousFields:   p,
-	// 	}
-	// 	if action == Deleted {
-	// 		trade.Op = "Delete"
-	// 	}
-	// 	return trade, nil
-	return nil, nil
+	trade := &Trade{
+		LedgerSequence:   txm.LedgerSequence,
+		TransactionIndex: txm.MetaData.TransactionIndex,
+		TransactionType:  txm.GetTransactionType().String(),
+		Op:               "Modify",
+		Paid:             paid,
+		Got:              got,
+		Giver:            v.HighLimit.Issuer,
+		Taker:            txm.Transaction.GetBase().Account,
+		// FinalFields:      v, => We Required this data as *Offer to calculate the price
+		// PreviousFields:   p,
+	}
+	if action == Deleted {
+		trade.Op = "Delete"
+	}
+	return trade, nil
 }
 
 func newTrade(txm *TransactionWithMetaData, i int) (*Trade, error) {
 	_, final, previous, action := txm.MetaData.AffectedNodes[i].AffectedNode()
 	v, ok := final.(*Offer)
 	if !ok || action == Created {
-		return newRippleState(txm, i)
+		// return newRippleState(txm, i)
+		return nil, nil
 	}
 	p := previous.(*Offer)
 	if p != nil && p.TakerGets == nil || p.TakerPays == nil {
